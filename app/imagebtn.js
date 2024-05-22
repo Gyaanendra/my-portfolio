ef24bba767413c0cf47c98af2f88c1299c0e500a
“Your <74192896+Gyaanendra@users.noreply.github.com>
2024-05-22 16:12:29 +0530

    jasbir

---
+++ app/imagebtn.js
@@ -0,0 +1,38 @@
+// components/ImageButton.js
+import React from "react";
+import css from "styled-jsx/css";
+
+// Define styles as a constant
+const buttonStyles = css`
+  .image-button {
+    border: none;
+    padding: 2;
+    background: none;
+    cursor: pointer;
+  }
+
+  .image {
+    display: block;
+    width: 200px; /* Adjust the size as needed */
+    height: auto;
+    border-radius: 12px; /* Optional: for rounded corners */
+    box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.2); /* Optional: for shadow effect */
+    transition: transform 0.2s ease; /* Optional: for hover effect */
+    background-color: #ebf6fa;
+  }
+
+  .image:hover {
+    transform: scale(1.05); /* Optional: for hover effect */
+  }
+`;
+
+const ImageButton = ({ src, alt, onClick }) => {
+  return (
+    <button className="image-button" onClick={onClick}>
+      <img src={src} alt={alt} className="image" />
+      <style jsx>{buttonStyles}</style>
+    </button>
+  );
+};
+
+export default ImageButton;
--- app/page.js
+++ app/page.js
@@ -1,15 +1,27 @@
 "use client";
 import React from "react";
-import { useState } from "react";
-
+import { useState, useEffect  } from "react";
+import ImageButton from "./imagebtn";
+import style from './page.module.css'
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    if (animate) {
+      const timer = setTimeout(() => setAnimate(false), 500); // Duration of animation
+      return () => clearTimeout(timer);
+    }
+  }, [animate]);
 
+  const handleClick = () => {
+    setCount(count + 1);
+    setAnimate(true);
+  };
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <p>hello</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <p className="style.btntext">You touched <p className={`${style.count} ${animate ? style.animate : ''}`}>{count}</p> times</p>
+      
+      <ImageButton src="./jasbir.png" alt="btn" onClick={handleClick} />
     </div>
   );
 }
